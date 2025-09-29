ebc32fa410a3cd4f96683544516bdd02c2fb2caa
Alex Jäger Peña <alexjaeger110@gmail.com>
2025-09-29 11:55:43 +0200

    Questionnaire items navigation working, first UI

--- app/src/main/java/de/syntax_institut/androidabschlussprojekt/navigation/AppStart.kt
+++ app/src/main/java/de/syntax_institut/androidabschlussprojekt/navigation/AppStart.kt
@@ -3,7 +3,6 @@ package de.syntax_institut.androidabschlussprojekt.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import de.syntax_institut.androidabschlussprojekt.ui.questionnaire.QuestionItem
 import de.syntax_institut.androidabschlussprojekt.ui.questionnaire.Questionnaire
 
 
